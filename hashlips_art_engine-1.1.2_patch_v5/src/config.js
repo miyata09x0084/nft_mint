2cf7fbb7db951dc71711b36873ec46442992e237
miyata09x0084 <59190800+miyata09x0084@users.noreply.github.com>
2022-12-07 10:12:26 +0900

    set_the_probability_of_the_image_appearence

--- hashlips_art_engine-1.1.2_patch_v5/src/config.js
+++ hashlips_art_engine-1.1.2_patch_v5/src/config.js
@@ -26,13 +26,9 @@ const layerConfigurations = [
   {
     growEditionSizeTo: 5,
     layersOrder: [
-      { name: "Background" },
-      { name: "Eyeball" },
-      { name: "Eye color" },
-      { name: "Iris" },
-      { name: "Shine" },
-      { name: "Bottom lid" },
-      { name: "Top lid" },
+      { name: "top" },
+      { name: "middle" },
+      { name: "bottom" },
     ],
   },
 ];
